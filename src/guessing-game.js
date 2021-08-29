5d8bab510441ed9c8ad8af471577fea1468c21e8
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-08-29 13:31:02 +0200

    update:finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
-
+ 
     setRange(min, max) {
 
+       this.minNumber = min;
+       this.maxNumber = max;
     }
 
     guess() {
-
+        this.answer = Math.round((this.maxNumber + this.minNumber) / 2);
+        return this.answer
     }
 
     lower() {
-
+        this.maxNumber = this.answer;
     }
 
     greater() {
-
+        this.minNumber = this.answer;
     }
 }
 
